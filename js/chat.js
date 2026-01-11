ec5169461ae699e80e7a358eac9c93d833dfa77d
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-01-11 16:05:36 +0300

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,1 +1,9 @@
+const channel = new BroadcastChannel("sadat-chat");
 
+function sendMessage(data) {
+  channel.postMessage(data);
+}
+
+channel.onmessage = (event) => {
+  displayMessage(event.data);
+};
